baa2acb682ce1dd5ad31c7405b4f297d208f0222
Jose De Las Salas <SueBuSo@Joses-MBP.attlocal.net>
2018-07-09 22:22:53 -0400

    added prelim moment.js code to format inputted date to reflect the format required for the query, still working out kinks of the query URL as i have yet to get a legit response.

--- assets/search.js
+++ assets/search.js
@@ -3,11 +3,22 @@ $(document).ready(function() {
 function searchNytApi () {
 
     var searchTerm = $("#searchTerm").val();
-    var startDate = $("#startDate"+"0101").val();
-    var endDate = $("#endDate"+"0101").val();
+    var startDate = $("#startDate").val();
+    var endDate = $("#endDate").val();
+//conversion of startDate/endDate through moment.js to YYYYMMDD:
 
-    //var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json=?q=" + searchTerm + "&fq=The NewYork Times&begin_date=" + startDate + "&end_date="+ endDate + "&api-key=70e55927c17d48d0b81b1eaff313a191";
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=hillary clinton&api-key=70e55927c17d48d0b81b1eaff313a191";
+    var begin_date = moment(startDate, "YYYY-MM-DD");
+    var end_date = moment(endDate, "YYYY-MM-DD");
+
+    console.log(searchTerm);
+    console.log(startDate);
+    console.log(endDate);
+    console.log(begin_date);
+    console.log(end_date);
+
+
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json=?q=" + searchTerm + "&fq=The NewYork Times&begin_date=" + begin_date + "&end_date="+ end_date + "&api-key=70e55927c17d48d0b81b1eaff313a191";
+    //var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=hillary clinton&api-key=70e55927c17d48d0b81b1eaff313a191";
     
     $.ajax({
         url:queryURL,
@@ -17,9 +28,8 @@ function searchNytApi () {
     .then(function(response) {
 
         console.log(response);
-
     })
 }
-searchNytApi ();
+$("#search").click(searchNytApi);
 
 });
